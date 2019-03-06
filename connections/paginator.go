f71c1f986a1363eb8bca1e1b5d2cb3fc8863409e
Alexey Kuznetsov <me@kuznetsoff.io>
2019-03-06 17:43:02 +0300

    Add cursor generation for gqlgen connections

---
+++ connections/paginator.go
@@ -0,0 +1,34 @@
+package connections
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// Paginator allows to paginate a collection
+type Paginator struct {
+	CursorPrefix string
+}
+
+// OffsetToCursor creates the cursor string from an offset
+func (p *Paginator) OffsetToCursor(offset int) string {
+	str := fmt.Sprintf("%v%v", p.CursorPrefix, offset)
+	return base64.StdEncoding.EncodeToString([]byte(str))
+}
+
+// CursorToOffset re-derives the offset from the cursor string.
+func (p *Paginator) CursorToOffset(cursor string) (int, error) {
+	str := ""
+	b, err := base64.StdEncoding.DecodeString(cursor)
+	if err == nil {
+		str = string(b)
+	}
+	str = strings.Replace(str, p.CursorPrefix, "", -1)
+	offset, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid cursor")
+	}
+	return offset, nil
+}
---
+++ connections/paginator_test.go
@@ -0,0 +1,48 @@
+package connections_test
+
+import (
+	"testing"
+
+	"github.com/semenovDL/gqlutils/connections"
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	prefix string
+	offset int
+	cursor string
+	err    error
+}
+
+var testCases = []testCase{
+	{"", -1, "LTE=", nil},
+	{"", 0, "MA==", nil},
+	{"", 1, "MQ==", nil},
+	{"", 2, "Mg==", nil},
+	{"", 100000, "MTAwMDAw", nil},
+	{"cursor:", 0, "Y3Vyc29yOjA=", nil},
+	{"cursor:", 1, "Y3Vyc29yOjE=", nil},
+}
+
+func TestConnections_Paginator_OffsetToCursor(t *testing.T) {
+	for _, c := range testCases {
+		p := connections.Paginator{CursorPrefix: c.prefix}
+		assert.Equal(t, c.cursor, p.OffsetToCursor(c.offset))
+	}
+}
+
+func TestConnections_Paginator_CursorToOffset(t *testing.T) {
+	for _, c := range testCases {
+		p := connections.Paginator{CursorPrefix: c.prefix}
+		value, err := p.CursorToOffset(c.cursor)
+		assert.Equal(t, c.offset, value)
+		assert.Equal(t, c.err, err)
+	}
+}
+
+func TestConnections_Paginator_CursorToOffset_BadCursor(t *testing.T) {
+	p := connections.Paginator{}
+	value, err := p.CursorToOffset("M{==")
+	assert.Equal(t, 0, value)
+	assert.Contains(t, err.Error(), "Invalid cursor")
+}
